e915c04a523cdf1231020fc1c64bf9fd9d8d27d5
jairajmatharu <40759086+jairajmatharu@users.noreply.github.com>
2018-07-20 12:58:38 +0100

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   blogPostDir: "posts", // The name of directory that contains your posts.
   lessonsDir: "lessons", // The name of the directory that contains lessons or docs.
-  siteTitle: "DMGT Docs Starter", // Site title.
-  siteTitleAlt: "Gatsby Starter Template for Creating Docs", // Alternative site title for SEO.
+  siteTitle: "DMGT Documents Repository", // Site title.
+  siteTitleAlt: "DMGT Documents Repository", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://ericwindmill.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "A GatsbyJS stater for creating Documentation or Tutorials. Using it as a template for DMGT", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "DMGT Documents Repository", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-82450300-1 ", // GA tracking ID.
@@ -49,5 +49,5 @@ module.exports = {
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0", // Used for setting manifest background color.
   // TODO: Move this literally anywhere better.
-  toCChapters: ["", "Chapter 1", "Chapter 2"] // Used to generate the Table Of Contents. Index 0 should be blank.
+  toCChapters: ["", "Introduction", "2nd section"] // Used to generate the Table Of Contents. Index 0 should be blank.
 };
