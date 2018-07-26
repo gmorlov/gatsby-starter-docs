6744097c98a9afd72f9dc002a981534f4f9d5310
jairajmatharu <40759086+jairajmatharu@users.noreply.github.com>
2018-07-26 19:44:32 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,23 +25,21 @@ class Index extends React.Component {
             </Hero>
           </IndexHeadContainer>
           <BodyContainer>
-            <font color="green"><H1>Welcome to the Green Book,a comprehensive guide to key processes and principles for our Operating Companies.</H1><font>
-              <p>This site is designed to empower you by clearly laying out the principles, practices and expectations that underpin our ways of working. You, our Operating Company leaders, need clarity, so you can go faster and make big decisions with absolute certainty about when DMGT&rsquo;s involvement is needed, and when it is not.
-              <br />
+            <font color="green"><H1>Welcome to the Green Book,a comprehensive guide to key processes and principles for our Operating Companies.</H1></font>
+            
+              <p>
+              This site is designed to empower you by clearly laying out the principles, practices and expectations that underpin our ways of working. You, our Operating Company leaders, need clarity, so you can go faster and make big decisions with absolute certainty about when DMGT&rsquo;s involvement is needed, and when it is not.
+              <br/>
               The working relationship between DMGT and its Operating Companies is, more than ever, critical to our success.
-              <br />
+              <br/>
               That relationship has evolved to meet the growing demands of the markets we operate in, and now is the right time to strengthen that relationship by setting out clear, mutual expectations of how we will work together.
-              <br />
+              <br/>
               By developing this understanding, we can progress more quickly, confidently and effectively towards our shared goals, deliver our Group-wide Performance Improvement Programme and position our businesses for sustainable, profitable growth.
-              <br />
+              <br/>
               Each business within our portfolio of number one brands has tremendous potential and opportunity ahead. DMGT is here to empower you, to support and encourage our shared values of entrepreneurialism, purpose and excellence, and to enable your success.
             </p>
  
-            <CtaButton to={'/lesson_one.md'}>The Introduction</CtaButton>
-
-            <div className="contributors">
-              
-            </div>
+            
           </BodyContainer>
         </main>
       </div>
