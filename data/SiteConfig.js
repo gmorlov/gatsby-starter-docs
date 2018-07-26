c60749b86f75b7271b93986cec946f51d1d0b256
jairajmatharu <40759086+jairajmatharu@users.noreply.github.com>
2018-07-26 19:01:03 +0100

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,18 +1,18 @@
 module.exports = {
   blogPostDir: "posts", // The name of directory that contains your posts.
   lessonsDir: "lessons", // The name of the directory that contains lessons or docs.
-  siteTitle: "DMGT Documents Repository", // Site title.
-  siteTitleAlt: "DMGT Documents Repository", // Alternative site title for SEO.
-  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteTitle: "DMGT Green Book", // Site title.
+  siteTitleAlt: "DMGT Green Book", // Alternative site title for SEO.
+  siteLogo: "/logos/dmgt-logo.jpg", // Logo used for SEO and manifest.
   siteUrl: "https://ericwindmill.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "DMGT Documents Repository", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "A guide for our Operating Companies 2018", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-82450300-1 ", // GA tracking ID.
   disqusShortname: "https-ericwindmill-github-io-gatsby-advanced-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
-  userName: "ThisUser", // Username to display in the author segment.
+  userName: "DMGT", // Username to display in the author segment.
   userTwitter: "ericwindmill", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "Portland, OR", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
@@ -45,7 +45,7 @@ module.exports = {
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2018. Advanced User", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0", // Used for setting manifest background color.
   // TODO: Move this literally anywhere better.
